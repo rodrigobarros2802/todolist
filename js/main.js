6e5fb98de6361d8960327730141b81fc3d422a80
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-08-22 14:50:07 -0300

    Adicionando seletores em JS

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelBtn = document.querySelector("#cancel-edit-btn");
+const todoList = document.querySelector("#todo-list");
