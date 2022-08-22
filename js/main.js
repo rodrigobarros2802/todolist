d6f7a473aa418e885e01d41ba88632ead384c523
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-08-22 15:34:18 -0300

    Criando lista JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+// Seleção dos Elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelBtn = document.querySelector("#cancel-edit-btn");
 const todoList = document.querySelector("#todo-list");
+
+// Funções
+const saveTodo = (text) => {
+    //Criando a variável todo
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    //Criando o título e colocando título dentro da variável Todo
+    const todoTittle = document.createElement("h3");
+    todoTittle.innerText = text;
+    todo.appendChild("todoTittle");
+
+    //Criando o Botão Concluído e colocando botão dentro da variável todo
+    const doneBtn = document.createElement("button");
+    doneBtn.innerHtml = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild("doneBtn");
+}
+
+
+// Eventos
+todoForm.addEventListener("submit", (e) => { //Criando evento submit
+    e.preventDefault(); //Evitando enviar ao clicar no botão
+    const inputValue = todoInput.value; //Criando a variável para capturar o valor do input
+    if(inputValue) { //Condicional If
+        saveTodo(inputValue); //Salvando os valores do input na Todo list
+    }
+})
